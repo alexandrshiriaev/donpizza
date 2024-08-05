7a8e0c030b6393a936143846764a6ce9c0fafdcb
Alexandr Shiriaev <alexandrshiriaev7@gmail.com>
2024-08-05 13:08:09 +0300

    fix(login): add suspense boundary for login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from '@/components/auth/login-form';
+import { Suspense } from 'react';
 
 export default async function LoginPage() {
     return (
         <div className="flex flex-col items-center justify-center w-full">
-            <LoginForm />
+            <Suspense fallback={<div>Loading...</div>}>
+                <LoginForm />
+            </Suspense>
         </div>
     );
 }
